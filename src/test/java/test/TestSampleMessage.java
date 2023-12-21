4e4ac56dd4e5bede29d027c3fb319449cd325ec8
raven <laingraven.java@gmail.com>
2023-12-21 17:03:42 +0700

    remove the animation when push component

--- src/test/java/test/TestSampleMessage.java
+++ src/test/java/test/TestSampleMessage.java
@@ -36,7 +36,7 @@ public class TestSampleMessage extends JFrame {
             GlassPanePopup.showPopup(new SimplePopupBorder(txt, "Sample Message", new SimplePopupBorderOption().useScroll(), actions, (controller, action) -> {
                 if (action == 1) {
                     String actions1[] = new String[]{"Back", "OK"};
-                    GlassPanePopup.push(new SimplePopupBorder(createSample(), "Test", actions1, (controller1, action1) -> {
+                    GlassPanePopup.push(new SimplePopupBorder(createSample(), "Test", new SimplePopupBorderOption().useScroll(),actions1, (controller1, action1) -> {
                         if (action1 == 0) {
                             GlassPanePopup.pop("pop");
                         }
@@ -52,25 +52,81 @@ public class TestSampleMessage extends JFrame {
     private Component createSample() {
         JPanel panel = new JPanel(new MigLayout("fill", "fill", ""));
         JTextPane txt = new JTextPane();
-        txt.setText("al/align alignx [aligny] or aligny/ay aligny or aligny/ax alignx\n" +
-                "Specifies the alignment for the laid out components as a group. If the total bounds of all laid out components does not fill the entire container the align value is used to position the components within the container without changing their relative positions. The alignment can be specified as a UnitValue or AlignKeyword. See above. If an AlignKeyword is used the \"align\" keyword can be omitted.\n" +
-                "Example: \"align 50% 50%\" or \"aligny top\" or \"alignx leading\" or \"align 100px\" or \"top, left\" or \"aligny baseline\"\n" +
-                "\n" +
-                "ltr/lefttoright or rtl/righttoleft\n" +
-                "Overrides the container's ComponentOrientation property for this layout. Normally this value is dependant on the Locale that the application is running. This constraint overrides that value.\n" +
-                "Example: \"ltr\" or \"lefttoright\" or \"rtl\"\n" +
-                "\n" +
-                "ttb/toptobottom or btt/bottomtotop\n" +
-                "Specifies if the components should be added in the grid bottom-to-top or top-to-bottom. This value is not picked up from the container and is top-to-bottom by default.\n" +
-                "Example: \"ttb\" or \"toptobottom\" or \"btt\"\n" +
-                "\n" +
-                "hidemode\n" +
-                "Sets the default hide mode for the layout. This hide mode can be overridden by the component constraint. The hide mode specified how the layout manager should handle a component that isn't visible. The modes are:\n" +
-                "0 - Default. Means that invisible components will be handled exactly as if they were visible.\n" +
-                "1 - The size of an invisible component will be set to 0, 0.\n" +
-                "2 - The size of an invisible component will be set to 0, 0 and the gaps will also be set to 0 around it.\n" +
-                "3 - Invisible components will not participate in the layout at all and it will for instance not take up a grid cell.\n" +
-                "Example: \"hidemode 1\"");
+        txt.setText("Apple\n" +
+                "Banana\n" +
+                "Orange\n" +
+                "Grapes\n" +
+                "Watermelon\n" +
+                "Pineapple\n" +
+                "Strawberry\n" +
+                "Blueberry\n" +
+                "Raspberry\n" +
+                "Mango\n" +
+                "Kiwi\n" +
+                "Papaya\n" +
+                "Peach\n" +
+                "Pear\n" +
+                "Plum\n" +
+                "Cherry\n" +
+                "Apricot\n" +
+                "Coconut\n" +
+                "Avocado\n" +
+                "Lemon\n" +
+                "Lime\n" +
+                "Grapefruit\n" +
+                "Cantaloupe\n" +
+                "Honeydew\n" +
+                "Fig\n" +
+                "Dates\n" +
+                "Pomegranate\n" +
+                "Cranberry\n" +
+                "Blackberry\n" +
+                "Goji berries\n" +
+                "Passionfruit\n" +
+                "Dragonfruit\n" +
+                "Starfruit\n" +
+                "Lychee\n" +
+                "Guava\n" +
+                "Persimmon\n" +
+                "Jackfruit\n" +
+                "Ackee\n" +
+                "Breadfruit\n" +
+                "Plantain\n" +
+                "Pine nuts\n" +
+                "Almonds\n" +
+                "Cashews\n" +
+                "Walnuts\n" +
+                "Pistachios\n" +
+                "Pecans\n" +
+                "Hazelnuts\n" +
+                "Macadamia nuts\n" +
+                "Chestnuts\n" +
+                "Brazil nuts\n" +
+                "Peanuts\n" +
+                "Sunflower seeds\n" +
+                "Pumpkin seeds\n" +
+                "Chia seeds\n" +
+                "Flaxseeds\n" +
+                "Sesame seeds\n" +
+                "Quinoa\n" +
+                "Bulgur\n" +
+                "Farro\n" +
+                "Barley\n" +
+                "Millet\n" +
+                "Buckwheat\n" +
+                "Oats\n" +
+                "Rice\n" +
+                "Couscous\n" +
+                "Wheatberries\n" +
+                "Spelt\n" +
+                "Teff\n" +
+                "Rye\n" +
+                "Corn\n" +
+                "Lentils\n" +
+                "Chickpeas\n" +
+                "Black beans\n" +
+                "Kidney beans\n" +
+                "Navy beans");
         txt.putClientProperty(FlatClientProperties.STYLE, "" +
                 "border:10,25,10,25;" +
                 "[light]foreground:lighten(@foreground,30%);" +
